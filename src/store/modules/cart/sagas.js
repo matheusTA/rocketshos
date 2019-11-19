d3b95715b76a9419818cc8c0b72488e3239a32b4
MatheusTA <matrajano12@gmail.com>
2019-11-19 17:05:37 -0300

    Verificando estoque ao adicionar produto

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -4,13 +4,23 @@ import { addToCartSuccess, updateAmount } from './actions';
 import { formatPrice } from '../../../util/format';
 
 function* addToCart({ id }) {
-  const productExists = yield select(
-    state => state.cart.find(p => p.id === id),
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
   );
 
-  if (productExists) {
-    const amount = productExists.amount + 1;
+  const stock = yield call(api.get, `/stock/${id}`);
 
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('ERRO');
+    return;
+  }
+
+  if (productExists) {
     yield put(updateAmount(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
@@ -18,13 +28,11 @@ function* addToCart({ id }) {
     const data = {
       ...response.data,
       amount: 1,
-      priceFormat: formatPrice(response.data.price)
-    }
+      priceFormat: formatPrice(response.data.price),
+    };
 
     yield put(addToCartSuccess(data));
   }
-
-
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
